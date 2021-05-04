af1f68b4115223ea8e6aad34fe4098a0f4d6cb97
nazrulnazx <nazrulhassan2312@gmail.com>
2021-05-04 13:53:50 -0700

    mongoDB connection part

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const connectDB      = require('./config/db');
+
+// Connect to database
+connectDB();
 
 const app     = express();
-
 const PORT    = process.env.PORT || 3000;
 
 
