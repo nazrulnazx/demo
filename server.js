e494b536410aedb0f57db78f3008f329165de20a
nazrulnazx <nazrulhassan2312@gmail.com>
2021-05-03 17:24:03 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app     = express();
+
+const PORT    = process.env.PORT || 3000;
+
+
+app.get('/',(req,res) => {
+    res.send('API Running');
+});
+
+
+app.listen(PORT,() => {
+    console.log(`App is listing on port ${PORT}`);
+});
