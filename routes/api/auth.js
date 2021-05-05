c60885e7bb7fa8ebead6e5dc2a95d5c8734a1e37
nazrulnazx <nazrulhassan2312@gmail.com>
2021-05-05 14:05:07 -0700

    demo Adding routes

---
+++ routes/api/auth.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router  = express.Router();
+
+
+/**
+ * Route    GET api/users
+ * DESC     Test Route
+ * Access   Public
+ */
+router.get('/',(req,res) => res.send('Auth Route') );
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-const express = require('express');
+const express        = require('express');
 const connectDB      = require('./config/db');
 
-// Connect to database
-connectDB();
 
+/**---------------------------------------------------
+ *  App Initialization
+ * ---------------------------------------------------
+ */
 const app     = express();
 const PORT    = process.env.PORT || 3000;
+connectDB();
 
 
-app.get('/',(req,res) => {
-    res.send('API Running');
-});
+
+/**---------------------------------------------------
+ *  DEFINING ROUTES
+ * ---------------------------------------------------
+ */
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/post',require('./routes/api/post'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/user',require('./routes/api/user'));
 
 
+app.use('',(req,res) => { res.send('OOOooppppssss  Wrong URL'); });
+
+/**---------------------------------------------------
+ *  Server Initialization
+ * ---------------------------------------------------
+ */
 app.listen(PORT,() => {
     console.log(`App is listing on port ${PORT}`);
 });
